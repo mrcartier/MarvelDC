2c7389e686e86f160069d507fd7006afbd773186
mrcartier <mrrcartier@gmail.com>
2017-02-08 17:54:11 -0500

    added tests

---
+++ app/src/androidTest/java/com/example/cartier/marveldc/RecyclerViewTest.java
@@ -0,0 +1,33 @@
+package com.example.cartier.marveldc;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by gerry on 2/8/2017.
+ */
+
+public class RecyclerViewTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public RecyclerViewTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        getActivity();
+    }
+
+    public void testClickAtPosition() {
+        //perform click in first RecyclerView element of Marvel Fragment
+        onView(withId(R.id.marvel_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+}
+
